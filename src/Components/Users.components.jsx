8d5c413a7cb44c86ec584c377ddd3a45c3bf47e3
Dhananjaya-dabeer <dabeerdhananja123@gmail.com>
2023-11-28 12:12:09 +0530

    added card UI

--- src/Components/Users.components.jsx
+++ src/Components/Users.components.jsx
@@ -3,35 +3,58 @@ import "./Users.css";
 import userIcon from "../assets/cornerIcon.png";
 import groupIcon from "../assets/moptroIcon.png";
 function Users() {
-    const [searchInput, setSearchInput] = useState("")
-    const employeData = [
-        {id: 1, name: "Arjun", DOB: "16-11-2000", Role: "Software Engineer"},
-        {id: 2, name: "Mahesh", DOB: "15-01-2000", Role: "Web Developer"},
-        {id: 3, name: "Chetan", DOB: "11-05-2001", Role: "Doctor"},
-    ] 
+  const [searchInput, setSearchInput] = useState("");
+  const employeData = [
+    { id: 1, name: "Arjun", DOB: "16-11-2000", Role: "Software Engineer" },
+    { id: 2, name: "Mahesh", DOB: "15-01-2000", Role: "Web Developer" },
+    { id: 3, name: "Chetan", DOB: "11-05-2001", Role: "Doctor" },
+  ];
 
-    const filteredArray = employeData.filter((employee) => (employee.name.toLowerCase().includes(searchInput.toLowerCase()))) 
+  const filteredArray = employeData.filter((employee) =>
+    employee.name.toLowerCase().includes(searchInput.toLowerCase())
+  );
   return (
     <div className="usersContainer">
       <div className="searchbar">
-        <input type="search" placeholder="Search with name"  onChange={(e) => setSearchInput( e.target.value)}/>
+        <input
+          type="search"
+          placeholder="Search with name"
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
       </div>
       <div className="users">
         <div className="cardscontainer">
-            {filteredArray.map((employee) => (
-                <div className="userParent">
-                <div className={employee.id % 2 === 0 ? "even" : "odd" } key={employee.id}>
-                    <p className="id">EMP ID : <span>{employee.id}</span></p>
-                    {/* <br /> */}
-                    <p className="name">Name : <span>{employee.name}</span></p>
-                 
-                    <p className="DOB">DOB : <span>{employee.DOB}</span></p>
-                  
-                    <p className="Role">Role : <span>{employee.Role}</span></p>
-                    <br />
+          {filteredArray.map((employee) => (
+            <div className="userParent">
+              <div
+                className={employee.id % 2 === 0 ? "even" : "odd"}
+                key={employee.id}
+              >
+                <div className="userdetailscontainer">
+                <p className="id">
+                  EMP ID : <span>{employee.id}</span>
+                </p>
+                {/* <br /> */}
+                <p className="name">
+                  Name : <span>{employee.name}</span>
+                </p>
+
+                <p className="DOB">
+                  DOB : <span>{employee.DOB}</span>
+                </p>
+
+                <p className="Role">
+                  Role : <span>{employee.Role}</span>
+                </p>
+                <br />
                 </div>
+                
+                <div className="cornernumber">
+                  <p>{employee.id}</p>
                 </div>
-            ))}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
